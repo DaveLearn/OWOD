562299294fc38ccab1d537d9db8aa08c070f3d18
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2021-08-26 22:45:55 +1000

    try train2 without lr schedule to match existing.

--- run.sh
+++ run.sh
@@ -20,9 +20,11 @@ task_1 () {
     # Task 1
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52125' --config-file ./configs/OWOD/t1/t1_train.yaml OUTPUT_DIR "./output/t1"
 
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t1/log.txt
     # No need to finetune in Task 1, as there is no incremental component.
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t1/t1_val.yaml OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t1_final" MODEL.WEIGHTS "./output/t1/model_final.pth"
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t1_final/log.txt
 
     python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t1/t1_test.yaml  OUTPUT_DIR "./output/t1_final" MODEL.WEIGHTS "./output/t1/model_final.pth"
 }
@@ -30,6 +32,7 @@ task_1 () {
 task_2 () {
     # Task 2
     cp -r ./output/t1 ./output/t2
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t2/log.txt
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t2/t2_train.yaml OUTPUT_DIR "./output/t2" MODEL.WEIGHTS "./output/t2/model_final.pth"
 
@@ -38,14 +41,15 @@ task_2 () {
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --config-file ./configs/OWOD/t2/t2_ft.yaml OUTPUT_DIR "./output/t2_ft" MODEL.WEIGHTS "./output/t2_ft/model_final.pth"
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t2/t2_val.yaml OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t2_final" MODEL.WEIGHTS "./output/t2_ft/model_final.pth"
-
+    
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t2_final/log.txt
     python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t2/t2_test.yaml OUTPUT_DIR "./output/t2_final" MODEL.WEIGHTS "./output/t2_ft/model_final.pth"
 }
 
 task_3 () {
     # Task 3
     cp -r ./output/t2_ft ./output/t3
-
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t3/log.txt
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t3/t3_train.yaml OUTPUT_DIR "./output/t3" MODEL.WEIGHTS "./output/t3/model_final.pth"
 
     cp -r ./output/t3 ./output/t3_ft
@@ -54,23 +58,26 @@ task_3 () {
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t3/t3_val.yaml OWOD.TEMPERATURE 1.5 OUTPUT_DIR "./output/t3_final" MODEL.WEIGHTS "./output/t3_ft/model_final.pth"
 
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t3_final/log.txt
     python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t3/t3_test.yaml OUTPUT_DIR "./output/t3_final" MODEL.WEIGHTS "./output/t3_ft/model_final.pth"
 }
 
 task_4 () {
     # Task 4
     cp -r ./output/t3_ft ./output/t4
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t4/log.txt
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t4/t4_train.yaml OUTPUT_DIR "./output/t4" MODEL.WEIGHTS "./output/t4/model_final.pth"
 
     cp -r ./output/t4 ./output/t4_ft
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --config-file ./configs/OWOD/t4/t4_ft.yaml OUTPUT_DIR "./output/t4_ft" MODEL.WEIGHTS "./output/t4_ft/model_final.pth"
-
+    
+    echo "Using code in commit: $(git log --pretty=reference -n 1)" >> ./output/t4_final/log.txt
     python tools/train_net.py --num-gpus 1 --eval-only --config-file ./configs/OWOD/t4/t4_test.yaml OUTPUT_DIR "./output/t4_final" MODEL.WEIGHTS "./output/t4_ft/model_final.pth"
 }
 
-task_1
+#task_1
 task_2
-task_3
-task_4
+#task_3
+#task_4
