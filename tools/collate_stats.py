e0773e96533cd550d355a0b2cbb83fc74413b74f
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2021-09-04 11:02:04 +1000

    add timings to collated stats

--- tools/collate_stats.py
+++ tools/collate_stats.py
@@ -5,11 +5,11 @@ import json
 script_path = os.path.dirname(os.path.abspath(__file__))
 
 output_path = os.path.abspath(sys.argv[1]) if len(sys.argv) > 1 else f"{script_path}/../output"
-outputs = ["t1_final", "t2_final", "t3_final", "t4_final"]
+result_outputs = ["t1_final", "t2_final", "t3_final", "t4_final"]
 
 collated = dict()
 
-for task_num,output in enumerate(outputs, start=1):
+for task_num,output in enumerate(result_outputs, start=1):
     logpath = output_path+"/"+output
     logfile = logpath+"/log.txt"
     outputfile = logpath+"/stats.json"
@@ -19,8 +19,28 @@ for task_num,output in enumerate(outputs, start=1):
         stats = json.load(statsfd)
         collated[f"Task {task_num}"]=stats
 
+train_outputs = ["t1", "t2", "t2_ft", "t3", "t3_ft", "t4", "t4_ft"]
+
+for task_num in range(1, 5):
+    for fine_tune in [False, True]:
+        if task_num == 1 and fine_tune:
+            continue
+        suffix = fine_tune and "_ft" or ""
+        output = f"t{task_num}{suffix}"
+        logpath = output_path+"/"+output
+        logfile = logpath+"/log.txt"
+        outputfile = logpath+"/timings.json"
+        print(f"Processing logfile: {logfile} -> {outputfile}")
+        os.system(f"python {script_path}/extract_timings.py < {logfile} > {outputfile}")
+        stat_key = fine_tune and "Fine Tune" or "Train"
+        with open(outputfile, 'r') as statsfd:
+            stats = json.load(statsfd)
+            if "Timings" not in collated[f"Task {task_num}"]:
+                collated[f"Task {task_num}"]["Timings"] = dict()
+            collated[f"Task {task_num}"]["Timings"][stat_key]=stats
+
+
 final_stats_path = f"{output_path}/stats.json"
 print(f"Outputting collated data to {final_stats_path}")
 with open(final_stats_path, "w") as statsfd:
     json.dump(collated, statsfd, indent=4)
-
