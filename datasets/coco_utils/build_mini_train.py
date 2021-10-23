a7a87c13e510917d4896c58470b7527d452787c8
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2021-10-23 12:37:36 +1000

    Actually subsample training set!

--- datasets/coco_utils/build_mini_train.py
+++ datasets/coco_utils/build_mini_train.py
@@ -10,7 +10,7 @@ def create_dataset(train_file, dest_file):
 
     random.shuffle(file_names)
 
-    to_keep = len(file_names) * 100 // percent_to_keep
+    to_keep = int(len(file_names) * (percent_to_keep / 100))
 
     with open(dest_file, "x") as df:
         df.writelines(file_names[:to_keep])
@@ -19,5 +19,5 @@ def create_dataset(train_file, dest_file):
 
 
 for task_num in range(1,5):
-    create_dataset(f"/home/david/dev/owod/datasets/VOC2007/ImageSets/Main/t{task_num}_train.txt", f"/home/david/dev/owod/datasets/VOC2007/ImageSets/Main/t{task_num}_{percent_to_keep}pct_train.txt")
+    create_dataset(f"/home/n11020211/OWOD/datasets/VOC2007/ImageSets/Main/t{task_num}_train.txt", f"/home/n11020211/OWOD/datasets/VOC2007/ImageSets/Main/t{task_num}_{percent_to_keep}pct_train.txt")
 
