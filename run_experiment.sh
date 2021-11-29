40205d7f94bf9a4a8a604011ec048c352114102e
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2021-11-29 16:15:21 +1000

    just run val

--- run_experiment.sh
+++ run_experiment.sh
@@ -53,17 +53,17 @@ collect_stats () {
 
 task_1 () {
 
-    if [[ -e $OUTDIR/t1 ]] || [[ -e $OUTDIR/t1_final ]]; then
-       echo "Task 1 folder already exists! - aborting"
-       exit 1
-    fi
+    #if [[ -e $OUTDIR/t1 ]] || [[ -e $OUTDIR/t1_final ]]; then
+   #    echo "Task 1 folder already exists! - aborting"
+  #     exit 1
+  #  fi
 
     # Task 1
-    python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52125' --config-file ./configs/OWOD/t1/t1_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t1"
+  #  python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52125' --config-file ./configs/OWOD/t1/t1_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t1"
 
-    echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t1/log.txt
+ #   echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t1/log.txt
     # No need to finetune in Task 1, as there is no incremental component.
-    clean_folder "$OUTDIR"/t1
+ #   clean_folder "$OUTDIR"/t1
 
     python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52133' --config-file ./configs/OWOD/t1/t1_val.yaml OWOD.TEMPERATURE 1.5 OUTPUT_DIR "$OUTDIR/t1_final" MODEL.WEIGHTS "$OUTDIR/t1/model_final.pth"
     #echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> ./output/t1_final/log.txt
@@ -75,18 +75,18 @@ task_1 () {
 
 task_2 () {
 
-    if [[ -e $OUTDIR/t2 ]] || [[ -e $OUTDIR/t2_ft ]] || [[ -e $OUTDIR/t2_final ]]; then
-       echo "Task 2 folder already exists! - aborting"
-       exit 1
-    fi
+  #  if [[ -e $OUTDIR/t2 ]] || [[ -e $OUTDIR/t2_ft ]] || [[ -e $OUTDIR/t2_final ]]; then
+  #     echo "Task 2 folder already exists! - aborting"
+  #     exit 1
+   # fi
 
     # Task 2
-    mkdir -p "$OUTDIR"/t2
-    echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t2/log.txt
+   # mkdir -p "$OUTDIR"/t2
+  #  echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t2/log.txt
 
-    python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t2/t2_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t2"
+  #  python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t2/t2_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t2"
 
-    clean_folder "$OUTDIR"/t2
+  #  clean_folder "$OUTDIR"/t2
     #cp -r -T "$OUTDIR"/t2 "$OUTDIR"/t2_ft
 
     #python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --config-file ./configs/OWOD/t2/t2_ft${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t2_ft" MODEL.WEIGHTS "$OUTDIR/t2_ft/model_final.pth"
@@ -103,17 +103,17 @@ task_2 () {
 
 task_3 () {
 
-    if [[ -e $OUTDIR/t3 ]] || [[ -e $OUTDIR/t3_ft ]] || [[ -e $OUTDIR/t3_final ]]; then
-       echo "Task 3 folder already exists! - aborting"
-       exit 1
-    fi
+   # if [[ -e $OUTDIR/t3 ]] || [[ -e $OUTDIR/t3_ft ]] || [[ -e $OUTDIR/t3_final ]]; then
+  #     echo "Task 3 folder already exists! - aborting"
+  #     exit 1
+  #  fi
 
     # Task 3
-    mkdir -p "$OUTDIR"/t3
-    echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t3/log.txt
-    python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t3/t3_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t3"
+  #  mkdir -p "$OUTDIR"/t3
+  #  echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t3/log.txt
+   # python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t3/t3_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t3"
 
-    clean_folder "$OUTDIR"/t3
+  #  clean_folder "$OUTDIR"/t3
    # cp -r "$OUTDIR"/t3 "$OUTDIR"/t3_ft
 
    # python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --config-file ./configs/OWOD/t3/t3_ft${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t3_ft" MODEL.WEIGHTS "$OUTDIR/t3_ft/model_final.pth"
@@ -130,18 +130,18 @@ task_3 () {
 
 task_4 () {
 
-    if [[ -e $OUTDIR/t4 ]] || [[ -e $OUTDIR/t4_ft ]] || [[ -e $OUTDIR/t4_final ]]; then
-       echo "Task 4 folder already exists! - aborting"
-       exit 1
-    fi
+   # if [[ -e $OUTDIR/t4 ]] || [[ -e $OUTDIR/t4_ft ]] || [[ -e $OUTDIR/t4_final ]]; then
+  #     echo "Task 4 folder already exists! - aborting"
+  #     exit 1
+  #  fi
 
     # Task 4
-    mkdir -p "$OUTDIR"/t4
-    echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t4/log.txt
+  #  mkdir -p "$OUTDIR"/t4
+  #  echo "Using code in commit: $(git log --pretty=oneline -n 1)" >> "$OUTDIR"/t4/log.txt
+#
+  #  python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t4/t4_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t4"
 
-    python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52127' --config-file ./configs/OWOD/t4/t4_train${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t4"
-
-    clean_folder "$OUTDIR"/t4
+  #  clean_folder "$OUTDIR"/t4
   #  cp -r "$OUTDIR"/t4 "$OUTDIR"/t4_ft
 
   #  python tools/train_net.py --num-gpus 1 --dist-url='tcp://127.0.0.1:52126' --config-file ./configs/OWOD/t4/t4_ft${SUFFIX}.yaml OUTPUT_DIR "$OUTDIR/t4_ft" MODEL.WEIGHTS "$OUTDIR/t4_ft/model_final.pth"
