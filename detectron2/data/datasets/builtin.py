7238caae731302769665fbc920ef6840157b6bc8
David Pershouse <d3.pershouse@hdr.qut.edu.au>
2021-12-06 15:53:00 +1000

    compare with 25 instances per class

--- detectron2/data/datasets/builtin.py
+++ detectron2/data/datasets/builtin.py
@@ -236,7 +236,7 @@ def register_all_pascal_voc(root):
         ("voc_coco_2007_val_10pct", "VOC2007", "all_task_val_10pct"),
         ("voc_coco_2007_val_25pct", "VOC2007", "all_task_val_25pct"),
         ("voc_coco_2007_val_50pct", "VOC2007", "all_task_val_50pct"),
-
+        ("voc_coco_2007_val_568", "VOC2007", "all_task_val_568"),
         ("t2_voc_coco_2007_train", "VOC2007", "t2_train"),
         ("t2_voc_coco_2007_mini_train", "VOC2007", "t2_25pct_train"),
         ("t2_voc_coco_2007_ft", "VOC2007", "t2_ft"),
